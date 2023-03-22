245030bcec72857ba631e1a24e849422304e8bf7
Yunho Jung <yunho_jung@tmax.co.kr>
2023-03-22 18:49:36 +0900

    feat: Search interfaces 모듈 컨트롤러 추가

---
+++ blog-search-api/blog-search-interfaces/src/main/java/com/openapi/blogsearch/interfaces/search/web/GetSearchController.java
@@ -0,0 +1,27 @@
+package com.openapi.blogsearch.interfaces.search.web;
+
+import com.openapi.blogsearch.application.search.port.dto.SearchData;
+import com.openapi.blogsearch.application.search.service.GetSearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class GetSearchController {
+
+    private final GetSearchService getSearchService;
+
+    @GetMapping("/search")
+    public SearchData listSearchResult(
+        @RequestParam String query,
+        @RequestParam(defaultValue = "accuracy") String sort,
+        @RequestParam(defaultValue = "1") int page,
+        @RequestParam(defaultValue = "10") int size
+    ) {
+        return getSearchService
+            .getSearchResult(query, sort, page, size);
+    }
+
+}
